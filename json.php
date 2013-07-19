7c2b30d14ab56396199fb5a9a12b118ae7972ca5
Chris Shumate <cshumate@atmosol.com>
2013-07-19 08:46:26 -0700

    updating ajax - removing searchParam from results array

--- json.php
+++ json.php
@@ -2,11 +2,9 @@
 if($_GET['s']){
    $searchParam = $_GET['s'];
     $arr=array(0=>array(
-        'searchParam'=>$searchParam,
-        'text'=>'apple',
+       'text'=>'apple',
         'url'=>'http://apple.com'),
         1=>array(
-            'searchParam'=>$searchParam,
             'text'=>'tacoma',
             'url'=>'http://tacoma.com')
     );
